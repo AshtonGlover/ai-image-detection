3bf87abdea6189447c784c490c35d3c1775b11a9
AshtonGlover <113224225+AshtonGlover@users.noreply.github.com>
2024-08-07 21:16:53 -0400

    feat: copying over old code

---
+++ code/model.py
@@ -0,0 +1,81 @@
+import tensorflow as tf
+import tensorflow_addons as tfa
+import matplotlib.pyplot as plt
+from preprocess import get_data
+import os
+import pickle
+
+class Model:
+    def __init__(self):
+        if (os.getcwd().split('/')[-1] != 'code'):
+            raise IsADirectoryError("Must be in code directory")
+        
+        self.model = tf.keras.Sequential([
+            tf.keras.layers.Input(shape=(32, 32, 3)), #change shape to (32, 32, 1) for ELA to work!!!!!! 
+                                                      #change shape to (32, 32, 3) for PRNU to work!!!!!!
+            
+            tf.keras.layers.Conv2D(32, (3, 3), padding='same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.ReLU(),
+            tf.keras.layers.MaxPooling2D((2, 2)),
+            tf.keras.layers.Dropout(0, 25),
+            
+            tf.keras.layers.Conv2D(64, (3, 3), padding='same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.ReLU(),
+            tf.keras.layers.MaxPooling2D((2, 2)),
+            tf.keras.layers.Dropout(0, 25),
+            
+            tf.keras.layers.Conv2D(64, (3, 3), padding='same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.ReLU(),
+            tf.keras.layers.MaxPooling2D((2, 2)),
+            tf.keras.layers.Dropout(0, 25),
+            
+            tf.keras.layers.Conv2D(64, (3, 3), padding='same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.ReLU(),
+            tf.keras.layers.MaxPooling2D((2, 2)),
+            tf.keras.layers.Dropout(0, 25),
+            
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(64),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.ReLU(),
+            tf.keras.layers.Dropout(0, 5),
+            
+            tf.keras.layers.Dense(1, activation='sigmoid')
+        ])
+
+        self.train_and_eval()
+
+    def train_and_eval(self):
+        train_data, train_labels, test_data, test_labels = get_data()
+
+        num_samples = len(train_data)
+        indices = tf.range(num_samples)
+        shuffled_indices = tf.random.shuffle(indices)
+
+        train_data = tf.gather(train_data, shuffled_indices)
+        train_labels = tf.gather(train_labels, shuffled_indices)
+
+        num_samples = len(test_data)
+        indices = tf.range(num_samples)
+        shuffled_indices = tf.random.shuffle(indices)
+
+        test_data = tf.gather(test_data, shuffled_indices)
+        test_labels = tf.gather(test_labels, shuffled_indices)
+
+        self.model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+        self.model.fit(train_data, train_labels, epochs=10, validation_data=(test_data, test_labels))
+
+        test_loss, test_accuracy = self.model.evaluate(test_data, test_labels)
+        print("Test Accuracy:", test_accuracy)
+
+        self.model.save("saved_model.h5")
+
+
+Model()
---
+++ code/preprocess.py
@@ -0,0 +1,67 @@
+import numpy as np 
+import tensorflow as tf
+import os
+import cv2
+import matplotlib.pyplot as plt
+from ela import ela
+from prnu import prnu
+
+def preprocess_image(image_path):
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    img = img.astype(np.float32) / 255.0 
+
+
+    # uncomment this line for ELA
+    img = ela(image_path) 
+
+    ## uncomment the 3 lines below for PRNU
+
+    # img = prnu(image_path)
+    img = cv2.resize(img, (255, 255))
+    img_width = img.size
+    # img = np.expand_dims(img, axis=-1)  # Add channel dimension
+    return img
+
+# def calculate_prnu(images):
+#     prnu = np.mean(images, axis=0) 
+#     return prnu
+
+# def extract_prnu_residuals(images, prnu):
+#     prnu_residuals = [img - prnu for img in images]
+#     return np.array(prnu_residuals)
+
+def get_images():
+    fake_test = []
+    for image in os.listdir("../data/test/FAKE"):
+        fake_test.append(preprocess_image(os.path.join("../data/test/FAKE", image)))
+
+    real_test = []
+    for image in os.listdir("../data/test/REAL"):
+        real_test.append(preprocess_image(os.path.join("../data/test/REAL", image)))
+
+    fake_train = []
+    for image in os.listdir("../data/train/FAKE"):
+        fake_train.append(preprocess_image(os.path.join("../data/train/FAKE", image)))
+
+    real_train = []
+    for image in os.listdir("../data/train/REAL"):
+        real_train.append(preprocess_image(os.path.join("../data/train/REAL", image)))
+
+    return fake_test, real_test, fake_train, real_train
+
+def get_data():
+    fake_test, real_test, fake_train, real_train = get_images()
+
+    train_data = fake_train + real_train
+    train_labels = np.array([0 for _ in range(len(fake_train))] + [1 for _ in range(len(real_train))])
+
+    test_data = fake_test + real_test
+    test_labels = np.array([0 for _ in range(len(fake_test))] + [1 for _ in range(len(real_test))])
+
+    # prnu_train = calculate_prnu(train_data)
+    # train_data = np.array(extract_prnu_residuals(train_data, prnu_train))
+
+    # prnu_test = calculate_prnu(test_data)
+    # test_data = np.array(extract_prnu_residuals(test_data, prnu_test))
+
+    return train_data, train_labels, test_data, test_labels
